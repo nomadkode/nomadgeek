ef3a7013fd52b1a12104c436b1ba495a79d5aca0
ramset <setiawanramadyan@gmail.com>
2021-05-06 20:16:16 +0700

    add dark mode button & add footer

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log('hello nomad');
+//* Dark mode
+function darkMode() {
+  const element = document.body;
+  element.classList.toggle('dark-mode');
+}
+
+//* Footer
+document.getElementById('year').innerHTML = new Date().getFullYear();

6c3f14caa505ba344b9117e5f38ee70c4f1a66f3
ramset <setiawanramadyan@gmail.com>
2021-05-09 00:03:21 +0700

    add gsap 3 animation to hero section

--- main.js
+++ main.js
@@ -9,3 +9,13 @@ document.getElementById('year').innerHTML = new Date().getFullYear();
 
 //* AOS
 AOS.init();
+
+//* GSAP 3
+gsap.from('.hero_text p, .btn', {
+  duration: 2,
+  x: -200,
+  opacity: 0,
+  ease: 'bounce',
+});
+
+gsap.from('.hero_img', { duration: 2, x: 200, opacity: 0, ease: 'bounce' });

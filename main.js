b5f8f7c6170edcdd36ec94668355967710ec3f17
ramset <setiawanramadyan@gmail.com>
2021-05-09 00:26:50 +0700

    edit AOS & GSAP props

--- main.js
+++ main.js
@@ -8,14 +8,23 @@ function darkMode() {
 document.getElementById('year').innerHTML = new Date().getFullYear();
 
 //* AOS
-AOS.init();
+AOS.init({
+  once: true,
+});
 
 //* GSAP 3
 gsap.from('.hero_text p, .btn', {
-  duration: 2,
+  duration: 1.5,
+  delay: 0.5,
   x: -200,
   opacity: 0,
   ease: 'bounce',
 });
 
-gsap.from('.hero_img', { duration: 2, x: 200, opacity: 0, ease: 'bounce' });
+gsap.from('.hero_img', {
+  duration: 1.5,
+  delay: 1,
+  x: 200,
+  opacity: 0,
+  ease: 'bounce',
+});
